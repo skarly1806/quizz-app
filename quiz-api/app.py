2864e679aa61fca3db9551ac717cd09f660d1eac
lemairec <charles.lemaire@edu.esiee.fr>
2022-12-13 23:22:00 +0100

    login en cours

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,5 +1,7 @@
 from flask_cors import CORS
 from flask import Flask
+from flask import Flask, request
+from jwt_utils import *
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +14,16 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+@app.route('/login', methods=['POST'])
+def Login():
+	payload = request.get_json()
+	if payload == "flask2023":
+		token = build_token()
+		return token, 200
+	else:
+		return 'Unauthorized', 401
+
 
 if __name__ == "__main__":
     app.run()
